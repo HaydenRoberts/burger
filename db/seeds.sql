65098a7a4742eb17922992180fdbd1122c347ebc
Hayden R <raydenhoberts@gmail.com>
2018-03-26 18:23:46 -0700

    burger game

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name) 
+VALUES ('Western Bacon');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Patty Melt');
+
+INSERT INTO burgers (burger_name) 
+VALUES ('Moco Loco');
